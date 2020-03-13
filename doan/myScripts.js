8469e06cb877fd546b5b6c146edb3f0200201d48
Noforgive <maithaison1998@gmail.com>
2020-03-13 20:44:39 +0700

    do an

---
+++ doan/myScripts.js
@@ -0,0 +1,86 @@
+function TrinhTham() {
+    document.getElementById("img1").src = "./image/TrinhThamImage/trinhtham1.jpg";
+    document.getElementById("img2").src = "./image/TrinhThamImage/trinhtham2.jpg";
+    document.getElementById("img3").src = "./image/TrinhThamImage/trinhtham3.jpg";
+    document.getElementById("img4").src = "./image/TrinhThamImage/trinhtham4.jpg";
+    document.getElementById("img5").src = "./image/TrinhThamImage/trinhtham5.jpg";
+    document.getElementById("img6").src = "./image/TrinhThamImage/trinhtham6.jpg";
+    document.getElementById("img7").src = "./image/TrinhThamImage/trinhtham7.jpg";
+    document.getElementById("img8").src = "./image/TrinhThamImage/trinhtham8.jpg";
+}
+
+function CoTich() {
+    document.getElementById("img1").src = "./image/CoTichImage/cotich1.jpg";
+    document.getElementById("img2").src = "./image/CoTichImage/cotich2.jpg";
+    document.getElementById("img3").src = "./image/CoTichImage/cotich3.jpg";
+    document.getElementById("img4").src = "./image/CoTichImage/cotich4.jpg";
+    document.getElementById("img5").src = "./image/CoTichImage/cotich5.jpg";
+    document.getElementById("img6").src = "./image/CoTichImage/cotich6.jpg";
+    document.getElementById("img7").src = "./image/CoTichImage/cotich7.jpg";
+    document.getElementById("img8").src = "./image/CoTichImage/cotich8.jpg";
+}
+
+function KhoaHoc() {
+    document.getElementById("img1").src = "./image/KhoaHocImage/khoahoc1.jpg";
+    document.getElementById("img2").src = "./image/KhoaHocImage/khoahoc2.jpg";
+    document.getElementById("img3").src = "./image/KhoaHocImage/khoahoc3.jpg";
+    document.getElementById("img4").src = "./image/KhoaHocImage/khoahoc4.jpg";
+    document.getElementById("img5").src = "./image/KhoaHocImage/khoahoc5.jpg";
+    document.getElementById("img6").src = "./image/KhoaHocImage/khoahoc6.jpg";
+    document.getElementById("img7").src = "./image/KhoaHocImage/khoahoc7.jpg";
+    document.getElementById("img8").src = "./image/KhoaHocImage/khoahoc8.jpg";
+}
+
+function VienTuong() {
+    document.getElementById("img1").src = "./image/VienTuongImage/vientuong1.jpg";
+    document.getElementById("img2").src = "./image/VienTuongImage/vientuong2.jpg";
+    document.getElementById("img3").src = "./image/VienTuongImage/vientuong3.jpg";
+    document.getElementById("img4").src = "./image/VienTuongImage/vientuong4.jpg";
+    document.getElementById("img5").src = "./image/VienTuongImage/vientuong5.jpg";
+    document.getElementById("img6").src = "./image/VienTuongImage/vientuong6.jpg";
+    document.getElementById("img7").src = "./image/VienTuongImage/vientuong7.jpg";
+}
+
+function NguNgon() {
+    document.getElementById("img1").src = "./image/NguNgonImage/ngungon1.jpg";
+    document.getElementById("img2").src = "./image/NguNgonImage/ngungon2.jpg";
+    document.getElementById("img3").src = "./image/NguNgonImage/ngungon3.jpg";
+    document.getElementById("img4").src = "./image/NguNgonImage/ngungon4.jpg";
+    document.getElementById("img5").src = "./image/NguNgonImage/ngungon5.jpg";
+    document.getElementById("img6").src = "./image/NguNgonImage/ngungon6.jpg";
+    document.getElementById("img7").src = "./image/NguNgonImage/ngungon7.jpg";
+    document.getElementById("img8").src = "./image/NguNgonImage/ngungon8.jpg";
+}
+
+function Show() {
+    var x = document.getElementsByClassName("a1");
+    if (x[0].style.display === "block") {
+        x[0].style.display = "none";
+    } else {
+        x[0].style.display = "block";
+    }
+
+    if (x[1].style.display === "block") {
+        x[1].style.display = "none";
+    } else {
+        x[1].style.display = "block";
+    }
+
+    if (x[2].style.display === "block") {
+        x[2].style.display = "none";
+    } else {
+        x[2].style.display = "block";
+    }
+
+    if (x[3].style.display === "block") {
+        x[3].style.display = "none";
+    } else {
+        x[3].style.display = "block";
+    }
+
+    if (x[4].style.display === "block") {
+        x[4].style.display = "none";
+    } else {
+        x[4].style.display = "block";
+    }
+}
